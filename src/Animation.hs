cd42776a3e3a19518b0adb4f83ba7a4e5d2e3585
arved <arved.friedemann@stoppe.de>
2022-01-22 20:22:25 +0100

    Made wiggle animation

--- src/Animation.hs
+++ src/Animation.hs
@@ -5,6 +5,7 @@ import Reanimate
 import Reanimate.Scene
 import Control.Lens
 import Control.Monad
+import Data.Text (Text)
 
 animateMain :: IO ()
 animateMain = putStrLn "Animating..." >> (reanimate  $ animation)
@@ -15,6 +16,7 @@ animation = env $ scene $ do
   symbols <- mapM oNew equation_symbs
   mapM_ oShow symbols
   symbols2 <- mapM oNew equation_symbs2
+  mapM (flip oShowWith wiggleAnim) symbols
   case (symbols, symbols2) of
     ([a,x,b,eq,a',b',x'],[a2,x2,b2,eq2,a'2,b'2,x'2]) -> do
       --fork $ oShowWith x oScaleOut
@@ -25,9 +27,9 @@ animation = env $ scene $ do
       moveAbsPerc b 1 (0.2,0.2)
 
       fork $ oHideWith x oScaleOut
-      fork $ oShowWith x2 oScaleIn
+      fork $ oShowWith x2 oDraw
       fork $ oHideWith x' oScaleOut
-      fork $ oShowWith x'2 oScaleIn
+      fork $ oShowWith x'2 oDraw
     (l,_) -> error $ "list length is " ++ show (length l)
   wait 1
 
@@ -36,13 +38,42 @@ moveAbsPerc obj d (rx, ry) = oTweenS obj d $ \t -> do
   oLeftX %= \origin -> fromToS origin (screenLeft + screenWidth * rx) t
   oTopY %= \origin -> fromToS origin (screenTop - screenHeight * ry) t
 
+--Taken from example
+highlightE :: Effect
+highlightE d t =
+  scale (1 + bellS 2 (t/d)*0.5) . rotate (wiggleS (t/d) * 20)
+
+--Taken from example
+-- s-curve, sin, s-curve
+wiggleS :: Signal
+wiggleS t
+  | t < 0.25  = curveS 2 (t*4)
+  | t < 0.75  = sin ((t-0.25)*2*pi+pi/2)
+  | otherwise = curveS 2 ((t-0.75)*4)-1
+
+wiggle :: Sprite s -> Scene s ()
+wiggle s = spriteE s $ overBeginning 1 $ aroundCenterE highlightE
+
+wiggleAnim :: SVG -> Animation
+wiggleAnim svg = animate $ \t -> (overBeginning 1 $ aroundCenterE highlightE) 1 t svg
+
 equation :: SVG
-equation =  translate 3 3 $ scale 2 $ center $
-  latexAlign "a\\ X\\ bc = a\\ bc\\ X"
+equation =  translate 3 3 $ stdLaTeX "a\\ X\\ bc = a\\ bc\\ X"
 
 equation2 :: SVG
-equation2 = scale 2 $ center $
-  latexAlign "a\\ bc\\ bc = a\\ bc\\ bc"
+equation2 = stdLaTeX "a\\ bc\\ bc = a\\ bc\\ bc"
+
+whiteFill :: SVG -> SVG
+whiteFill = withFillColor "white"
+
+whiteOutl :: SVG -> SVG
+whiteOutl = withStrokeColor "white"
+
+white :: SVG -> SVG
+white = whiteFill . whiteOutl
+
+stdLaTeX :: Text -> SVG
+stdLaTeX = withStrokeWidth 0.001 . white . center . latexAlign
 
 equation_symbs :: [SVG]
 equation_symbs = [snd $ splitGlyphs i equation | i <- [[0],[1],[2,3],[4],[5],[6,7],[8]]]
@@ -54,7 +85,7 @@ env :: Animation -> Animation
 env = addStatic bg
 
 bg :: SVG
-bg = mkBackground "lightblue"
+bg = mkBackground "black"
 
 
 {-
