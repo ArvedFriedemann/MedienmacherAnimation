8e506a4d6aa6b7fb53bd82bc31367b46723da327
arved <arved.friedemann@stoppe.de>
2022-01-27 23:52:45 +0100

    made noun and predicate wiggle

--- src/ScriptedAnimation.hs
+++ src/ScriptedAnimation.hs
@@ -32,6 +32,15 @@ initialProblem = scene $ do
   fork $ oShow sent2
   moveAbsPerc sent2 1 (0.3,0)
 
+  oHide sent2
+  (sent2r,sent2s) <- oNewTup $ splitGlyphs [8] $
+    svgTranslateTopLeftPerc (0.3,0) $
+    stdLaTeX $ "sentence\\ S?"
+  oShow sent2r
+  oShowWith sent2s wiggleAnim
+  mapM oHide [sent2r,sent2s]
+  oShow sent2
+
   sent3 <- oNew $ stdLaTeX $ "sentence\\ (the\\ X\\ Y)\\ \\vdash"
   fork $ moveAbsPerc sent2 1 (0.2,0)
   setTopLeftPerc (0.2,lineThicknessThin) sent3
@@ -55,13 +64,39 @@ initialProblem = scene $ do
   (ruler, rulex) <- oNewTup $ splitGlyphs [12] $
     svgTranslateTopLeftPerc (0,lineThicknessThin) $
     stdLaTeX $ "sentence\\ (the\\ X\\ Y)\\ \\vdash"
-  (tailr, tailx) <- oNewTup $ splitGlyphs [0] $
+  (tailr, tailx) <- oNewTup $ splitGlyphs ([0]++[4..7]) $
+    svgTranslateTopLeftPerc (lineThicknessThin,lineThicknessThin*2) $
+    stdLaTeX $ "X\\ is\\ a\\ noun,\\ Y\\ is\\ a\\ verb"
+  (_, tailx) <- oNewTup $ splitGlyphs ([0]) $
+    svgTranslateTopLeftPerc (lineThicknessThin,lineThicknessThin*2) $
+    stdLaTeX $ "X\\ is\\ a\\ noun,\\ Y\\ is\\ a\\ verb"
+  (_, tailnoun) <- oNewTup $ splitGlyphs ([4..7]) $
     svgTranslateTopLeftPerc (lineThicknessThin,lineThicknessThin*2) $
     stdLaTeX $ "X\\ is\\ a\\ noun,\\ Y\\ is\\ a\\ verb"
   mapM oHide [sent3,sent4,sent5]
-  mapM oShow [sentr,sentx,ruler,rulex, tailr, tailx]
-  mapM (fork . flip oShowWith wiggleAnim) [sentx,rulex,tailx]
+  mapM oShow [sentr,sentx,ruler,rulex, tailr, tailx,tailnoun]
+  waitOn $ mapM (fork . flip oShowWith wiggleAnim) [sentx,rulex,tailx,tailnoun]
+  mapM oHide [sentr,sentx,ruler,rulex, tailr, tailx,tailnoun]
 
+  (sentr2, sentx2) <- oNewTup $ splitGlyphs [13] $
+    svgTranslateTopLeftPerc (0,0) $
+    stdLaTeX $ "sentence\\ (the\\ X\\ Y)?"
+  (ruler2, rulex2) <- oNewTup $ splitGlyphs [13] $
+    svgTranslateTopLeftPerc (0,lineThicknessThin) $
+    stdLaTeX $ "sentence\\ (the\\ X\\ Y)\\ \\vdash"
+  (tailr2, tailx2) <- oNewTup $ splitGlyphs ([9]++[13..16]) $
+    svgTranslateTopLeftPerc (lineThicknessThin,lineThicknessThin*2) $
+    stdLaTeX $ "X\\ is\\ a\\ noun,\\ Y\\ is\\ a\\ verb"
+  (_, tailx2) <- oNewTup $ splitGlyphs ([9]) $
+    svgTranslateTopLeftPerc (lineThicknessThin,lineThicknessThin*2) $
+    stdLaTeX $ "X\\ is\\ a\\ noun,\\ Y\\ is\\ a\\ verb"
+  (_, tailnoun2) <- oNewTup $ splitGlyphs ([13..16]) $
+    svgTranslateTopLeftPerc (lineThicknessThin,lineThicknessThin*2) $
+    stdLaTeX $ "X\\ is\\ a\\ noun,\\ Y\\ is\\ a\\ verb"
+  mapM oHide [sent3,sent4,sent5]
+  mapM oShow [sentr2,sentx2,ruler2,rulex2, tailr2, tailx2,tailnoun2]
+  waitOn $ mapM (fork . flip oShowWith wiggleAnim) [sentx2,rulex2,tailx2,tailnoun2]
+  --mapM oHide [sentr2,sentx2,ruler2,rulex2, tailr2, tailx2,tailnoun2]
 
   wait 10
 
